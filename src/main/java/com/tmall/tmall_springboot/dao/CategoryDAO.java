c975aa9539c83c825628a178651e36f5fa3f534d
Xuanzhihao <xuanzhihao1997@outlook.com>
2019-12-02 17:13:12 +0800

    test

--- src/main/java/com/tmall/tmall_springboot/dao/CategoryDAO.java
+++ src/main/java/com/tmall/tmall_springboot/dao/CategoryDAO.java
@@ -1,6 +1,7 @@
 package com.tmall.tmall_springboot.dao;
 
 import com.tmall.tmall_springboot.pojo.Category;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryDAO extends JpaRepository<Category,Integer> {
--- src/main/java/com/tmall/tmall_springboot/dao/PropertyDAO.java
+++ src/main/java/com/tmall/tmall_springboot/dao/PropertyDAO.java
@@ -1,4 +1,11 @@
 package com.tmall.tmall_springboot.dao;
 
-public interface PropertyDAO {
+import com.tmall.tmall_springboot.pojo.Category;
+import com.tmall.tmall_springboot.pojo.Property;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyDAO extends JpaRepository<Property,Integer> {
+    Page<Property> findByCategory(Category category, Pageable pageable);
 }
