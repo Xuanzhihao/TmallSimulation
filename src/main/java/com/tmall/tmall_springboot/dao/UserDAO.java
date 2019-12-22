ec0a2b5cc0ce55bc6931159930d1b6029b2cfcd7
Xuanzhihao <xuanzhihao1997@foxmail.com>
2019-12-22 21:46:29 +0800

    add register.html

--- src/main/java/com/tmall/tmall_springboot/dao/UserDAO.java
+++ src/main/java/com/tmall/tmall_springboot/dao/UserDAO.java
@@ -4,4 +4,5 @@ import com.tmall.tmall_springboot.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDAO  extends JpaRepository<User, Integer> {
+    User findByName(String name);
 }
--- src/main/java/com/tmall/tmall_springboot/web/ForeRESTController.java
+++ src/main/java/com/tmall/tmall_springboot/web/ForeRESTController.java
@@ -1,11 +1,17 @@
 package com.tmall.tmall_springboot.web;
 
 import com.tmall.tmall_springboot.pojo.Category;
+import com.tmall.tmall_springboot.pojo.User;
 import com.tmall.tmall_springboot.service.CategoryService;
 import com.tmall.tmall_springboot.service.ProductService;
+import com.tmall.tmall_springboot.service.UserService;
+import com.tmall.tmall_springboot.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -17,6 +23,9 @@ public class ForeRESTController {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/forehome")
     public Object home() {
         List<Category> cs= categoryService.list();
@@ -24,5 +33,24 @@ public class ForeRESTController {
         productService.fillByRow(cs);
         categoryService.removeCategoryFromProduct(cs);
         return cs;
+    }
+
+    @PostMapping("/foreregister")
+    public Object register(@RequestBody User user){
+        String name = user.getName();
+        String password = user.getPassword();
+        name = HtmlUtils.htmlEscape(name);
+        user.setName(name);
+        boolean exist = userService.isExist(name);
+
+        if (exist){
+            String message ="用户名已经被使用,不能使用";
+            return Result.fail(message);
+        }
+
+        user.setPassword(password);
+        userService.add(user);
+        return Result.success();
+
     }
 }
