afee2d42e338397e3afa07adce4ebf5a6d4a5e65
Xuanzhihao <xuanzhihao1997@foxmail.com>
2020-02-07 16:27:20 +0800

    add the cart

---
+++ src/main/java/com/tmall/tmall_springboot/config/WebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.tmall.tmall_springboot.config;
+
+import com.tmall.tmall_springboot.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+    @Bean
+    public LoginInterceptor getLoginIntercepter() {
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getLoginIntercepter())
+                .addPathPatterns("/**");
+    }
+}
